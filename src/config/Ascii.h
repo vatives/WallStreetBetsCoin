c609019400385a1eb5849e572e139eef3344a384
Kiyoto Tomioka <kiyototomioka@gmail.com>
2021-01-31 01:58:28 -0800

    Retail Investors Commit

---
+++ src/config/Ascii.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2021, WallStreetBets Coin
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string windowsAsciiArt =
+ 	"\n                                              \n"
+        "   __       __   ______   _______    ______     \n" 
+	"  /  |  _  /  | /      \ /       \  /      \    \n"
+	"  $$ | / \ $$ |/$$$$$$  |$$$$$$$  |/$$$$$$  |   \n"
+	"  $$ |/$  \$$ |$$ \__$$/ $$ |__$$ |$$ |  $$/    \n"
+	"  $$ /$$$  $$ |$$      \ $$    $$< $$ |         \n"
+	"  $$ $$/$$ $$ | $$$$$$  |$$$$$$$  |$$ |   __    \n"
+	"  $$$$/  $$$$ |/  \__$$ |$$ |__$$ |$$ \__/  |   \n"
+	"  $$$/    $$$ |$$    $$/ $$    $$/ $$    $$/    \n"
+	"  $$/      $$/  $$$$$$/  $$$$$$$/   $$$$$$/     \n"
+        "                                                \n";
+
+
+const std::string nonWindowsAsciiArt = 
+        "\n                                              \n"
+        "   __       __   ______   _______    ______     \n" 
+	"  /  |  _  /  | /      \ /       \  /      \    \n"
+	"  $$ | / \ $$ |/$$$$$$  |$$$$$$$  |/$$$$$$  |   \n"
+	"  $$ |/$  \$$ |$$ \__$$/ $$ |__$$ |$$ |  $$/    \n"
+	"  $$ /$$$  $$ |$$      \ $$    $$< $$ |         \n"
+	"  $$ $$/$$ $$ | $$$$$$  |$$$$$$$  |$$ |   __    \n"
+	"  $$$$/  $$$$ |/  \__$$ |$$ |__$$ |$$ \__/  |   \n"
+	"  $$$/    $$$ |$$    $$/ $$    $$/ $$    $$/    \n"
+	"  $$/      $$/  $$$$$$/  $$$$$$$/   $$$$$$/     \n"
+        "                                                \n";
+
+
+/* Windows has some characters it won't display in a terminal. If your ascii
+   art works fine on Windows and Linux terminals, just replace 'asciiArt' with
+   the art itself, and remove these two #ifdefs and above ascii arts */
+#ifdef _WIN32
+const std::string asciiArt = windowsAsciiArt;
+#else
+const std::string asciiArt = nonWindowsAsciiArt;
+#endif
