e447c762c35bb96fffdd2ef47a5dee0dbafee9ff
Gerardo Gómez <34922579+cryptodeveloperbro@users.noreply.github.com>
2021-02-05 18:43:08 -0800

    Update CryptoNoteCheckpoints.h

--- src/config/CryptoNoteCheckpoints.h
+++ src/config/CryptoNoteCheckpoints.h
@@ -27,5 +27,11 @@ struct CheckpointData {
   const char* blockId;
 };
 
-const std::initializer_list<CheckpointData> CHECKPOINTS = {};
+const std::initializer_list<CheckpointData> CHECKPOINTS = {
+        {1, "b9f397f541f5a5a9349e4760a5121e8aa1bbe73155400431860380972ca395aa"},
+        {10, "fe33576ba5a9f522ae7889f09144d8e68da02e3edcd3ab0e6d176a3aa0681aac"},
+        {100, "d7e2d1726788b106407a304b9ffbebf30d659fea4ff978bc320262e6bfb00a98"},
+        {1000, "36d65a110552e197651e3fb4f8d88a6d6b8a306800eaa10b955538a06eb38671"},
+        {1500, "b1b32be559c9242de21edfc7edffe3cb226bf42c7430d080f801972e7841e104"},
+        {2500, "1fef1a554c5245897aa053a85ca5fd4649ec80d5473e6661fb8b61e1b0544116"}};
 }
